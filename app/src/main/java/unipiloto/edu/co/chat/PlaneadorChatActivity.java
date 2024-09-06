c31cdc4bb720033b5cfdd9afbee08a6e37d1f9d9
JuanMesa527 <mesacalderon@gmail.com>
2024-09-06 15:34:07 -0500

    Initial commit

---
+++ app/src/main/java/unipiloto/edu/co/chat/PlaneadorChatActivity.java
@@ -0,0 +1,50 @@
+package unipiloto.edu.co.chat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class PlaneadorChatActivity extends AppCompatActivity {
+
+    private ArrayList<String> historial;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        historial = new ArrayList<>();
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_planeador_chat);
+        Intent intent = getIntent();
+        if (intent.getStringArrayListExtra("historial") != null) {
+            historial = intent.getStringArrayListExtra("historial");
+        }
+        TextView messageView = (TextView)findViewById(R.id.message);
+        for (String message : historial) {
+            messageView.setText(messageView.getText() + "\n" + message);
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText)findViewById(R.id.writtenMessage);
+        String messageText = "Planeador: "+messageView.getText().toString();
+        historial.add(messageText);
+        Intent intent = new Intent(this, CiudadanoChatActivity.class);
+        intent.putExtra("historial",historial);
+        startActivity(intent);
+    }
+}
